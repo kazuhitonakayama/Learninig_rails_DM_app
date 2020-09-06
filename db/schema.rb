42a85784c23747e6dc15bd61b4dd7419054178ae
kazuhitonakayama <firstjin19990101@gmail.com>
2020-09-06 23:36:49 +0900

    Make Notification model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_092457) do
+ActiveRecord::Schema.define(version: 2020_09_06_143607) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 2020_09_05_092457) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visitor_id"
+    t.integer "visited_id"
+    t.integer "room_id"
+    t.integer "message_id"
+    t.string "action"
+    t.boolean "checked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "rooms", force: :cascade do |t|
